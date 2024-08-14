f483388d9ecb860cc68f03a1abfcc825f64871d7
priyadarshini122 <priyadarshinigaddala52@gmail.com>
2024-08-14 06:50:10 +0000

    first commit

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,29 @@
+// Write your code here.
+import './index.css'
+
+const NavBar = props => {
+  const {score, topScore, winStatus} = props
+  return (
+    <nav className="navbar">
+      <div className="nav-items-container">
+        <div className="emoji-logo-name-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+            alt="emoji logo"
+            className="emoji-logo-navbar"
+          />
+          <h1 className="game-name">Emoji Game</h1>
+        </div>
+        {winStatus === null ? (
+          <>
+            //Fragment
+            <p className="score-text">Score: {score}</p>
+            <p className="score-text">Top Score: {topScore}</p>
+          </>
+        ) : null}
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
